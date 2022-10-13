beb7fdfd6637edbcc3115768b8d06e231b83765e
JosephM0129 <115744277+JosephM0129@users.noreply.github.com>
2022-10-13 18:25:48 -0300

    Update HeaderCSV.py

--- HeaderCSV.py
+++ HeaderCSV.py
@@ -1,6 +1,6 @@
 # Leer la primera línea de CSV
 import csv  
-with open(r"C:\Users\Joseph\OneDrive - Lisicki Litvin y Asociados\GIR Y PRUEBAS DE ESTRÉS 2022\PPC GIR\CE Crédito\Bases\202112_CE_Detalle_Consolidado_Sc.txt") as csvfile:  
+with open(r"C:\Users\file.csv") as csvfile:  
     data = csv.DictReader(csvfile, delimiter = '|')
     for row in data:
         print(', '.join(row))
