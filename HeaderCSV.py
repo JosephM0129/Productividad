d1fa8c26481b726598af384a54434572dd07322a
JosephM0129 <115744277+JosephM0129@users.noreply.github.com>
2023-01-06 12:44:39 -0300

    Update HeaderCSV.py

--- HeaderCSV.py
+++ HeaderCSV.py
@@ -1,6 +1,6 @@
 # Leer la primera línea de CSV
 import csv  
-with open(r"C:\Users\file.csv") as csvfile:  
+with open(r"C:\Users\file.csv", encoding = 'utf8') as csvfile:  
     data = csv.DictReader(csvfile, delimiter = '|')
     for row in data:
         print(', '.join(row))
